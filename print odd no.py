48c3562ceca41e160641bcf4ab25d34030831a9e
swapna1029 <53587556+swapna1029@users.noreply.github.com>
2019-08-12 23:29:52 -0900

    Update print odd no.py

--- print odd no.py
+++ print odd no.py
@@ -1,3 +1,4 @@
-num= input("Type in: ")
-odd = [i for i in num.split(',') if (int(i) % 2 != 0)]
-print((" ").join(odd))
+l=[1,2,3,4,5,6,7,8]
+for i in range(0,len(l)):
+    if l[i]%2!=0:
+        print(l[i]**2)
